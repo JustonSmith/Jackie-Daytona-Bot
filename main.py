9003292d8e9a934a522e04fc87b84739a55e768a
JustonSmith <75957152+JustonSmith@users.noreply.github.com>
2021-11-11 01:29:53 -0600

    forlorn

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from apikeys import *
 
 client = discord.Client()
 
-sad_words = ["sad", "depressed", "unhappy", "angry", "miserable", "bummed", "irritated", "mad", "pissed", "distressed", "troubled", "heartbroken", "hopeless", "melancholy", "gloomy", "glum", "woeful", "disappointed", "bleak",   ]
+sad_words = ["sad", "depressed","forlorn", "unhappy", "angry", "miserable", "bummed", "irritated", "mad", "pissed", "distressed", "troubled", "heartbroken", "hopeless", "melancholy", "gloomy", "glum", "woeful", "disappointed", "bleak",   ]
 
 starter_encouragements = ["If it aint broke, dont fix it.", "Nothing ventured, nothing gained", "Haste makes waste.", "One swallow doesnt make a summer.", "Practice makes perfect", "A rolling stone gathers no moss.", "Jack of all trades, master of none.", "Descretion is the better part of valor.", "A watched pot never boils.", "Dont look a gift horse in the mouth.", "People who live in glass houses shouldnt throw stones.", "Necessity is the mother of invention.", "A stitch in time saves nine", "Dont cross that bridge until you come to it.", "Every dog has its day.", "The squeaky wheel gets the grease.", "Dont bite the hand that feeds you.", "Too many cooks spoil the broth.", "Dont put all your eggs in one basket", "You can lead a horse to water but you cant make it drink", "A bird in hand is better than two in the bush.", "A chain is only as strong as its weakest link", "Two is company, three is a crowd.", "Fools rush in where angels fear to tread."]
 
